4cadb5c11e47cd5e94ea3794d990094dcc868c7b
camiloreyes1 <jreye236@fiu.edu>
2023-09-12 19:40:18 -0400

    fetch fixed commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,12 @@
-import { useState, useEffect } from 'react'
-import ReactDOM from 'react-dom/client'
-import './App.css'
+import React from 'react'
+import Weather from './pages/Weather'
 
-function App() {
-  
-  const [endPoint, setEndPoint] = useState('')
-  const [container, setContainer] = useState([])
-
-  useEffect (() => {
-    fetchMe
-  }, [endPoint])
-
-
-  const fetchMe = () => {
-
-  fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=+${endPoint}`, {
-    "method": "GET",
-    "headers": {
-      'X-RapidAPI-Key': 'c12851fd20msh21d808cb922c788p103241jsn91fe9186b684',
-      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-    }
-    })
-
-    .then(response => {
-      return(response.json())
-    })
-
-    .then(data => {
-      setContainer(data.location)
-    })
-
-    .catch(err => {
-      console.log(err);
-    })
-  }
-
-    const onChangeHandler = (e) => {
-      setEndPoint(e.target.value)
-    }
-
-    const submitHandler = e => {
-      e.preventDefault()
-    }
-
-  return ( 
-    <div>
-
-      <form onSubmit={submitHandler}>
-
-        <input type='text' value={endPoint} onChange={onChangeHandler} />
-        <button type='submit'>Submit</button>
-
-      </form>
-
-      {container.map((item) => {
-        return (
-          <div>
-            <p>{item.name}</p>
-            <p>{item.country}</p>
-
-          </div>
-        )
-      })}
-    </div>
+const App = () => {
+  return (
+	<div>
+    <Weather/>
+  </div>
   )
-};
+}
 
 export default App
---
+++ src/pages/Weather.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Weather = () => {
+
+    //  the actual    temp variable the set variable is what is changing
+    const [response, setResponse] = useState([]);
+    const fetchWeater = async () => {
+        try {
+            const res = await axios.get('https://weatherapi-com.p.rapidapi.com/current.json', {
+                headers: {
+                    'X-RapidAPI-Key': 'c12851fd20msh21d808cb922c788p103241jsn91fe9186b684',
+                    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+                },
+                params: { q: '53.1,-0.13' },
+            });
+
+            setResponse(res.data);
+            console.log(res.data);
+
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    // it's a parameter for what happens after the function is done
+    useEffect(() => {
+        // Trigger the API Call
+        fetchWeater();
+    }, []);
+
+
+    return (
+
+        <div>
+          <p>Location: {response.location.name} </p>
+          {/* <p>Temperature: {response.current.temp_f}°C</p>          */}
+        </div>
+    )
+};
+
+
+export default Weather
